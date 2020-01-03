d389f754f79719b593ad7f2a0569df8cf0ee24ed
Olivia Golec <oliviagolec5@gmail.com>
2020-01-03 13:07:11 -0500

    yes

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const readlineSync = require("readline-sync");
+
+var taskInput=document.getElementById("new-task");//Add a new task.
+var addButton=document.getElementsByTagName("button")[0];//first button
